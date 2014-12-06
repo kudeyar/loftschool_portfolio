fcdff4f8d4923a6473618a04d08390d9582c3795
Serega <Serega@192.168.56.1>
2014-12-07 00:15:35 +0400

    header

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+
+        <header>
+            <div class="header_wrapper">
+                <div class="loft-text">
+                    <font class="header_text">LOFTSCHOOL</font>
+                    <div class="header_title_line"></div>
+                    <font class="header_text_mini">комплексное обучение web разработке</font>
+                </div>
+
+                <div class="sn">
+                    <a href="vk.com" class="vk"></a>
+                    <a href="vk.com" class="twi"></a>
+                    <a href="vk.com" class="fb"></a>
+                    <a href="vk.com" class="git"></a>
+                </div>
+                <div class="header_line"></div>
+            </div>
+        </header>
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
