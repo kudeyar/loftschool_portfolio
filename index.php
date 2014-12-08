039e3872a5e83862509c3c357155deeaff56fba1
Serega <kudeyar-rolevik@yandex.ru>
2014-12-09 02:17:04 +0400

    about page the end

--- index.php
+++ index.php
@@ -40,18 +40,96 @@
                 <div class="header_line"></div>
             </div>
         </header>
-        
-        <wrapper>
-            <menu>
-                <ul id="navigation">
-                    <li><a href="/about">Обо мне</a></li>
-                    <div class="line"></div>
-                    <li><a href="/works">Мои работы</a></li>
-                    <div class="line"></div>
-                    <li><a href="/contacts">Связаться со мной</a></li>
-                </ul>
-            </menu>
-        </wrapper>
+        <div class="megawrap">
+            <wrapper>
+                <menu>
+                    <ul id="navigation">
+                        <li><a href="/about">Обо мне</a></li>
+                        <div class="line"></div>
+                        <li><a href="/works">Мои работы</a></li>
+                        <div class="line"></div>
+                        <li><a href="/contacts">Связаться со мной</a></li>
+                    </ul>
+                </menu>
+
+                <content>
+                    <content_title>
+                        <div class="content_title">Основная информация</div>
+                    </content_title>
+                    <infa>
+                        <img src="/img/photo.jpg" class="photo">
+
+                        <font class="title_bold">Меня зовут:</font>
+                        <font class="text">Карпов Сергей Алексеевич</font>
+                        <br />
+                        <font class="title_bold">Мой возраст:</font>
+                        <font class="text">23 года</font>
+                        <br />
+                        <font class="title_bold">Мой город:</font>
+                        <font class="text">Ставрополь</font>
+                        <br />
+                        <font class="title_bold">Моя специализация:</font>
+                        <font class="text">FRONTEND разработчик</font>
+                        <br />
+                        <font class="title_bold">Ключевые навыки:</font>
+                        <div class="group_navyk">
+                        <div class="navyk">html</div> 
+                        <div class="navyk">css</div> 
+                        <div class="navyk">js </div>
+                        <div class="navyk">php </div>
+                        <div class="navyk">git </div>
+                        <div class="navyk">ajax </div>
+                        <div class="navyk">Zend </div>
+                        </div>
+                    </infa>
+                </content>
+
+                <content>
+                    <content_title>
+                        <div class="content_title">Опыт работы</div>
+                    </content_title>
+                    <infa>
+                        <div class="work">
+                            <img src="/img/work.png" class="work_img">
+                            <font class="work_place">"Невинномысский химический колледж" - Программист</font><br />
+                            <font class="work_time">Сентябрь 2011 - Июнь 2012</font>
+                        </div>
+                        <div class="work">
+                            <img src="/img/work.png" class="work_img">
+                            <font class="work_place">"ООО MeetToday Russia" - Программист</font><br />
+                            <font class="work_time">Октябрь 2013 - Декабрь 2014</font>
+                        </div>
+                    </infa>
+                </content>
+
+                <content>
+                    <content_title>
+                        <div class="content_title">Образование</div>
+                    </content_title>
+                    <infa>
+                        <div class="work">
+                            <img src="/img/teach.png" class="work_img">
+                            <font class="work_place">Средне-профессильное. НХК</font><br />
+                            <font class="work_time">Сентябрь 2006 - Июнь 2011</font>
+                        </div>
+                        <div class="work">
+                            <img src="/img/teach.png" class="work_img">
+                            <font class="work_place">Незаконченное высшее. ТТИ ЮФУ</font><br />
+                            <font class="work_time">Сентябрь 2011 - по настоящее время</font>
+                        </div>
+                        <div class="work">
+                            <img src="/img/course.png" class="work_img course">
+                            <font class="work_place">Курсы Loftschool.ru</font><br />
+                            <font class="work_time">Декабрь 2014 - по настоящее время</font>
+                        </div>
+                    </infa>
+                </content>
+            </wrapper>
+        </div>
+        <footer>
+            <div class="fline"></div>
+            © 2014, Это мой сайт, пожалуйста, не копируйте и не воруйте его
+        </footer>
 
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
